68d92bc634c8dc58b927ecc000ad52cd4765927e
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-21 21:10:32 +0100

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,101 +1,97 @@
 #include "main.h"
 
 /**
- * print_char - it is printing one char.
+ * print_char - Prints one char.
+ * @ptr: A pointer that points at the arg.
  *
- * @ptr: a pointer that point at the arg.
- * Return: the number of characters printed, it is always 1.
+ * Return: The number of characters printed, always 1.
  */
-
 int print_char(va_list ptr)
 {
-    char str1 = (char)va_arg(ptr, int);
+	char str1 = (char)va_arg(ptr, int);
 
-    write(1, &str1, 1);
-    return (1);
+	write(1, &str1, 1);
+	return (1);
 }
 
 /**
- * print_string - it is printing one string.
+ * print_string - Prints one string.
+ * @ptr: A pointer that points at the arg.
  *
- * @ptr: a pointer that point at the arg.
- * Return: the lengh of string.
+ * Return: The length of the string.
  */
-
 int print_string(va_list ptr)
 {
-    int charcount = 0;
-    char *str2 = va_arg(ptr, char *);
+	int charcount = 0;
+	char *str2 = va_arg(ptr, char *);
 
-    if (str2 == NULL)
-    {
-        write(1, "(null)", 6);
-        return (6);
-    }
+	if (str2 == NULL)
+	{
+		write(1, "(null)", 6);
+		return (-1);
+	}
 
-    while (*str2)
-    {
-        write(1, str2, 1);
-        str2 = str2 + 1;
-        charcount++;
-    }
+	while (*str2)
+	{
+		write(1, str2, 1);
+		str2 = str2 + 1;
+		charcount++;
+	}
 
-    return (charcount);
+	return (charcount);
 }
 
 /**
- * print_pourcentage - it is printing a %.
+ * print_pourcentage - Prints a %.
  *
- * void : nothing.
- * Return: the number of characters printed, and it is always 1.
+ * Return: The number of characters printed, always 1.
  */
 int print_pourcentage(void)
 {
-    char str3 = '%';
+	char str3 = '%';
 
-    write(1, &str3, 1);
-    return (1);
+	write(1, &str3, 1);
+	return (1);
 }
 
 /**
- * _printf - it is the printf function.
+ * _printf - The printf function.
+ * @format: A constant char.
  *
- * @format: a cte char.
- * Return: the number of characters printed.
+ * Return: The number of characters printed.
  */
-
 int _printf(const char *format, ...)
 {
-    int charcount = 0;
-    va_list ptr;
+	int charcount = 0;
+	va_list ptr;
 
-    va_start(ptr, format);
-    if (format == NULL)
-        return (-1);
-    while (*format)
-    {
-        if ((*format == '%') && (*(format + 1) == 'c'))
-        {
-            charcount += print_char(ptr);
-            format += 2;
-        }
-        else if ((*format == '%') && (*(format + 1) == 's'))
-        {
-            charcount += print_string(ptr);
-            format += 2;
-        }
-        else if ((*format == '%') && (*(format + 1) == '%'))
-        {
-            charcount += print_pourcentage();
-            format += 2;
-        }
-        else
-        {
-            write(1, format, 1);
-            charcount++;
-            format++;
-        }
-    }
-    va_end(ptr);
-    return (charcount);
+	va_start(ptr, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if ((*format == '%') && (*(format + 1) == 'c'))
+		{
+			charcount += print_char(ptr);
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == 's'))
+		{
+			charcount += print_string(ptr);
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == '%'))
+		{
+			charcount += print_pourcentage();
+			format += 2;
+		}
+		else
+		{
+			write(1, format, 1);
+			charcount++;
+			format++;
+		}
+	}
+	va_end(ptr);
+	return (charcount);
 }
