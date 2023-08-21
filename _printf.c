109cd902138782c5b2431b73ae5479e9c0795745
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-21 23:00:24 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * print_char - it is printing one char.
+ * print_char - Prints one char.
+ * @ptr: A pointer that points at the arg.
  *
- * @ptr: a pointer that point at the arg.
- * Return: the number of characters printed, it is always 1.
+ * Return: The number of characters printed, always 1.
  */
-
 int print_char(va_list ptr)
 {
-    char c = (char)va_arg(ptr, int);
-    write(1, &c, 1);
-    return (1);
+	char str1 = (char)va_arg(ptr, int);
+
+	write(1, &str1, 1);
+	return (1);
 }
 
-
 /**
- * print_string - it is printing one string.
+ * print_string - Prints one string.
+ * @ptr: A pointer that points at the arg.
  *
- * @ptr: a pointer that point at the arg.
- * Return: the lengh of string.
+ * Return: The length of the string.
  */
-
 int print_string(va_list ptr)
 {
 	int charcount = 0;
@@ -44,10 +42,9 @@ int print_string(va_list ptr)
 }
 
 /**
- * print_pourcentage - it is printing a %.
+ * print_pourcentage - Prints a %.
  *
- * void : nothing.
- * Return: the number of characters printed, and it is always 1.
+ * Return: The number of characters printed, always 1.
  */
 int print_pourcentage(void)
 {
@@ -58,58 +55,43 @@ int print_pourcentage(void)
 }
 
 /**
- * _printf - it is the printf function.
+ * _printf - The printf function.
+ * @format: A constant char.
  *
- * @format: a cte char.
- * Return: the number of characters printed.
+ * Return: The number of characters printed.
  */
-
-/* ... (rest of your code for print_char, print_string, and print_pourcentage) ... */
-
-/**
- * _printf - it is the printf function.
- *
- * @format: a cte char.
- * Return: the number of characters printed.
- */
-
 int _printf(const char *format, ...)
 {
-    int charcount = 0;
-    va_list ptr;
+	int charcount = 0;
+	va_list ptr;
 
-    va_start(ptr, format);
-    if (format == NULL)
-        return (-1);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    charcount += print_char(ptr);
-                    break;
-                case 's':
-                    charcount += print_string(ptr);
-                    break;
-                case '%':
-                    charcount += print_pourcentage();
-                    break;
-                default:
-                    write(1, format - 1, 1);
-                    charcount++;
-                    break;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            charcount++;
-        }
-        format++; // Move this line outside the switch block
-    }
-    va_end(ptr);
-    return (charcount);
+	va_start(ptr, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if ((*format == '%') && (*(format + 1) == 'c'))
+		{
+			charcount += print_char(ptr);
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == 's'))
+		{
+			charcount += print_string(ptr);
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == '%'))
+		{
+			charcount += print_pourcentage();
+			format += 2;
+		}
+		else
+		{
+			write(1, format, 1);
+			charcount++;
+			format++;
+		}
+	}
+	va_end(ptr);
+	return (charcount);
 }
