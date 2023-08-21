3ed359e6999eaec320574761526ce2c96ac0dcc7
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-21 22:42:51 +0100

    task1

--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 int print_char(va_list ptr);
 int print_string(va_list ptr);
 int print_pourcentage(void);
+int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_number(int n);
 int _printf(const char *format, ...);
