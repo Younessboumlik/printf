66c8d0473a34ace5d73d07ea2932cbe994ec8f63
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-21 20:32:44 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -78,16 +78,24 @@ int _printf(const char *format, ...)
 		{
 			charcount += print_char(ptr);
 			format += 2;
+			break;
 		}
 		else if ((*format == '%') && (*(format + 1) == 's'))
 		{
 			charcount += print_string(ptr);
 			format += 2;
+			break;
 		}
 		else if ((*format == '%') && (*(format + 1) == '%'))
 		{
 			charcount += print_pourcentage();
 			format += 2;
+			break;
+		}
+		else if(*format == '%')
+		{
+			format++;
+			break;
 		}
 		else
 		{
