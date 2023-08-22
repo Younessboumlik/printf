6c2cc0ffdb0482acc21fbd95d96070291c804ac3
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-22 17:39:13 +0100

    Update task_2_func.c

--- task_2_func.c
+++ task_2_func.c
@@ -33,3 +33,38 @@ int print_binary(va_list ptr)
 
 	return (charcount);
 }
+
+int print_int(va_list ptr)
+{
+    int int_input;
+    unsigned int int_in, int_temp, i, div, isneg;
+
+    int_input = va_arg(arguments, int);
+    isneg = 0;
+    if (int_input < 0)
+    {
+        int_in = int_input * -1;
+        write(1, "-", 1);
+        isneg = 1;
+    }
+    else
+    {
+        int_in = int_input;
+    }
+
+    int_temp = int_in;
+    div = 1;
+
+    while (int_temp > 9)
+    {
+        div *= 10;
+        int_temp /= 10;
+    }
+
+    for (i = 0; div > 0; div /= 10, i++)
+    {
+        char c = ((int_in / div) % 10) + '0';
+        write(1, &c, 1);
+    }
+    return (i + isneg);
+}
