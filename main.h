60252c7b5bf4171f4901479de95ff354fe2473c0
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-22 02:56:28 +0100

    Update main.h

--- main.h
+++ main.h
@@ -9,10 +9,8 @@
 int print_char(va_list ptr);
 int print_string(va_list ptr);
 int print_pourcentage(void);
-int print_buf(char *buf, unsigned int nbuf);
-unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int handle_format(char c, va_list ptr, char *buf, unsigned int ibuf);
- int print_int(va_list arguments, char *buf, unsigned int ibuf)
+int print_int(va_list arguments)
 int _printf(const char *format, ...);
 int print_binary(va_list ptr);
 
