50aaa637a398c1dfed94d75156f25ec1a653536d
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-22 14:28:08 +0100

    Update main.h

--- main.h
+++ main.h
@@ -6,8 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int _puts(char *str);
-int _putchar(char c);
+int _puts(va_list ptr);
+int _putchar(va_lisrt ptr);
 int print_pourcentage(void);
 int print_binary(va_list ptr);
 int _printf(const char *format, ...);
