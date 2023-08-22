8b64e2e2b883bfe99a54f3c06c08a09c40f3e499
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-22 14:35:21 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -14,6 +14,10 @@ int _printf(const char *format, ...)
 	va_start(ptr, format);
 	if (format == NULL)
 		return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+        	return (-1);
+    	if (format[0] == '%' && format[1] == ' ' && !format[2])
+        	return (-1);
 	while (*format)
 	{
 		if (*format == '%')
