6e4ad8d39ed25cce43591c6a0a6db26c418d66c9
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-22 03:19:22 +0100

    fixing files

--- _printf.c
+++ _printf.c
@@ -7,43 +7,10 @@
  * Return: The number of characters printed.
  */
 
-int handle_format(char c, va_list ptr, char *buf, unsigned int ibuf)
-{
-	int charcount = 0;
-
-	switch (c)
-	{
-		case 'c':
-			charcount += print_char(ptr);
-			break;
-		case 's':
-			charcount += print_string(ptr);
-			break;
-		case '%':
-			charcount += print_pourcentage();
-			break;
-		case 'b':
-			charcount += print_binary(ptr);
-			break;
-		case 'd':
-		case 'i':
-			charcount += print_int(ptr, buf, ibuf);
-			break;
-		default:
-			write(1, &c, 1);
-			charcount++;
-	}
-
-	return charcount;
-}
-
-
 int _printf(const char *format, ...)
 {
 	int charcount = 0;
 	va_list ptr;
-	char buf[1024];
-	unsigned int ibuf = 0;
 
 	va_start(ptr, format);
 	if (format == NULL)
@@ -53,7 +20,24 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++; /* Increment to the specifier character */
-			charcount += handle_format(*format, ptr, buf, ibuf);
+			switch (*format)
+			{
+				case 'c':
+					charcount += print_char(ptr);
+					break;
+				case 's':
+					charcount += print_string(ptr);
+					break;
+				case '%':
+					charcount += print_pourcentage();
+					break;
+				case 'b':
+					charcount += print_binary(ptr);
+					break;
+				default:
+					write(1, format, 1);
+					charcount++;
+			}
 			format++; /* Move to the character after the specifier */
 		}
 		else
